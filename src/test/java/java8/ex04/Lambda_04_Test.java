fc40cdaf0dc596bbbad7d57c1a433fc173ab8baf
Alex <alex.thepaut33@gmail.com>
2019-02-13 16:09:19 +0100

    Résolution exo 4 et fin du TP

--- src/test/java/java8/ex04/Lambda_04_Test.java
+++ src/test/java/java8/ex04/Lambda_04_Test.java
@@ -1,9 +1,12 @@
 package java8.ex04;
 
 
+import java8.data.Account;
 import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -17,14 +20,17 @@ public class Lambda_04_Test {
     // tag::interfaces[]
     interface GenericPredicate<T> {
         // TODO
+    	boolean test(T p);
     }
 
     interface GenericMapper<T, E> {
         // TODO
+    	E map(T p);
     }
 
     interface Processor<T> {
         // TODO
+    	void process(T p);
     }
     // end::interfaces[]
 
@@ -48,17 +54,28 @@ public class Lambda_04_Test {
         private FuncCollection<T> filter(GenericPredicate<T> predicate) {
             FuncCollection<T> result = new FuncCollection<>();
             // TODO
+            for (T p : this.list) {
+    			if(predicate.test(p)){
+    				result.add(p);
+    			}
+    		}
             return result;
         }
 
         private <E> FuncCollection<E> map(GenericMapper<T, E> mapper) {
             FuncCollection<E> result = new FuncCollection<>();
             // TODO
+            for (T t : this.list) {
+            	result.add((E) mapper.map(t));
+    		}
             return result;
         }
 
         private void forEach(Processor<T> processor) {
            // TODO
+        	for (T person : this.list) {
+         	   processor.process((T) person);
+            }
         }
         // end::methods[]
 
@@ -76,12 +93,19 @@ public class Lambda_04_Test {
 
         personFuncCollection
                 // TODO filtrer, ne garder uniquement que les personnes ayant un age > 50
-                .filter(null)
+                .filter(p -> p.getAge() > 50)
                 // TODO transformer la liste de personnes en liste de comptes. Un compte a par défaut un solde à 1000.
-                .map(null)
+                .map(p -> {
+                	Account a = new Account();
+                	a.setBalance(1000);
+                	a.setOwner((Person) p);
+                	return a;
+                })
                 // TODO vérifier que chaque compte a un solde à 1000.
                 // TODO vérifier que chaque titulaire de compte a un age > 50
-                .forEach(null);
+                .forEach(p -> {	assertTrue(((Account) p).getBalance() == 1000);
+            					assertTrue(((Account) p).getOwner().getAge() > 50); 
+                });
     }
     // end::test_filter_map_forEach[]
 
@@ -96,22 +120,32 @@ public class Lambda_04_Test {
         // TODO créer un variable filterByAge de type GenericPredicate
         // TODO filtrer, ne garder uniquement que les personnes ayant un age > 50
         // ??? filterByAge = ???;
+        GenericPredicate<Person> filterByAge = p -> p.getAge() > 50;
 
         // TODO créer un variable mapToAccount de type GenericMapper
         // TODO transformer la liste de personnes en liste de comptes. Un compte a par défaut un solde à 1000.
         // ??? mapToAccount = ???;
+        GenericMapper<Person, Account> mapToAccount = p -> {
+													        	Account a = new Account();
+													        	a.setBalance(1000);
+													        	a.setOwner((Person) p);
+													        	return a;
+													        };
 
         // TODO créer un variable verifyAccount de type GenericMapper
         // TODO vérifier que chaque compte a un solde à 1000.
         // TODO vérifier que chaque titulaire de compte a un age > 50
         // ??? verifyAccount = ???;
-
-        /* TODO Décommenter
+		Processor<Account> verifyAccount = p -> {	assertTrue(((Account) p).getBalance() == 1000);
+													assertTrue(((Account) p).getOwner().getAge() > 50); 
+												};
+		
+        /* TODO Décommenter*/
         personFuncCollection
                 .filter(filterByAge)
                 .map(mapToAccount)
                 .forEach(verifyAccount);
-        */
+        
     }
     // end::test_filter_map_forEach_with_vars[]
 
